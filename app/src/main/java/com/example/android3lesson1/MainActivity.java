e716ee75d64bb5447cf956fe97ffb1b218af5b5f
mrasms <shurik96kg@gmail.com>
2022-05-04 00:59:19 +0600

    Логика из класса "MainActivity" перемещена в класс "MainViewModel", попробовал поработать с "MutableLiveData".

--- app/src/main/java/com/example/android3lesson1/MainActivity.java
+++ app/src/main/java/com/example/android3lesson1/MainActivity.java
@@ -1,32 +1,40 @@
 package com.example.android3lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.android3lesson1.adapter.DataAdapter;
+import com.example.android3lesson1.recycler.adapter.DataAdapter;
 import com.example.android3lesson1.databinding.ActivityMainBinding;
-import com.example.android3lesson1.models.DataModel;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private ArrayList<DataModel> list = new ArrayList<>();
+    private MainViewModel mainViewModel = new MainViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        addObserver();
         setListener();
     }
 
-
-    private void initAdapter() {
-        DataAdapter adapter = new DataAdapter(list);
-        binding.recycler.setAdapter(adapter);
+    private void addObserver() {
+        mainViewModel.mutableLiveData.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    DataAdapter adapter = new DataAdapter(mainViewModel.list);
+                    binding.recycler.setAdapter(adapter);
+                }
+            }
+        });
     }
 
     private void setListener() {
@@ -34,13 +42,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 binding.btnGetData.setVisibility(View.INVISIBLE);
-                list.add(new DataModel("Привет"));
-                list.add(new DataModel("qwert"));
-                list.add(new DataModel("asdfg"));
-                list.add(new DataModel("zxcvb"));
-                list.add(new DataModel("/.,mn"));
-                list.add(new DataModel("lkjhgf"));
-                initAdapter();
+                mainViewModel.onClickBtn();
             }
         });
     }
