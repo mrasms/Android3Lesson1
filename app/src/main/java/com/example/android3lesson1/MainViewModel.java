36cb58e253674cf19500a010c5cfed0ac08e5e3c
mrasms <shurik96kg@gmail.com>
2022-05-09 03:18:12 +0600

    MutableLiveData теперь работает не с булеаном а с Интегером, изменено название метода заполняющего список на более логичное, убрана ненужная переменная btnIsClicked.

--- app/src/main/java/com/example/android3lesson1/MainViewModel.java
+++ app/src/main/java/com/example/android3lesson1/MainViewModel.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 
 public class MainViewModel extends ViewModel {
 
-    public MutableLiveData<Boolean> mutableLiveData = new MutableLiveData<>();
     public ArrayList<DataModel> list = new ArrayList<>();
-    public boolean btnIsClicked = false;
+    public MutableLiveData<Integer> mutableLiveData = new MutableLiveData<>();
 
-    public void onClickBtn() {
-        btnIsClicked = true;
-        mutableLiveData.setValue(btnIsClicked);
+    public void fillList() {
+
         list.add(new DataModel("Привет"));
         list.add(new DataModel("qwert"));
         list.add(new DataModel("asdfg"));
         list.add(new DataModel("zxcvb"));
         list.add(new DataModel("/.,mn"));
         list.add(new DataModel("lkjhgf"));
+
+        mutableLiveData.setValue(list.size());
 
     }
 
