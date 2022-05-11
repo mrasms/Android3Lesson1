19ba14e5bd71bd3943a45b4bae51e29bcfa6bfc4
mrasms <shurik96kg@gmail.com>
2022-05-11 22:17:24 +0600

    добавлена и реализована Навигация, всё лишнее пеенесено в соответствующий фрагмент

---
+++ app/src/main/java/com/example/android3lesson1/ui/activity/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android3lesson1.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.android3lesson1.R;
+import com.example.android3lesson1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupNavigation();
+
+    }
+
+    private void setupNavigation() {
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController);
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.detailFragment, R.id.homeFragment).build();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
