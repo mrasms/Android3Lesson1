d5cafeaa339514bce5b990513f985f372e2fdb45
mrasms <shurik96kg@gmail.com>
2022-05-11 22:19:48 +0600

    Фрагмент добавлен, в нём принимаются и отображаются данные из выбранного item фрагмента HomeFragment

---
+++ app/src/main/java/com/example/android3lesson1/ui/fragments/detail/DetailFragment.java
@@ -0,0 +1,41 @@
+package com.example.android3lesson1.ui.fragments.detail;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android3lesson1.databinding.FragmentDetailBinding;
+
+
+public class DetailFragment extends Fragment {
+
+    private FragmentDetailBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentDetailBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setText();
+    }
+
+    private void setText() {
+        String data = DetailFragmentArgs.fromBundle(getArguments()).getModel().getText();
+        binding.tvDetailData.setText(data);
+
+    }
+
+}
+
+
