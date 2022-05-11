f7dfde8f88e0ba60896deac12b7041d60ac46296
mrasms <shurik96kg@gmail.com>
2022-05-11 22:17:45 +0600

    всё лишнее перенесено в соответствующий фрагмент

--- app/src/main/java/com/example/android3lesson1/MainViewModel.java
+++ app/src/main/java/com/example/android3lesson1/MainViewModel.java
@@ -9,16 +9,5 @@ import java.util.ArrayList;
 
 public class MainViewModel extends ViewModel {
 
-    public ArrayList<DataModel> list = new ArrayList<>();
-    public MutableLiveData<ArrayList<DataModel>> mutableLiveData = new MutableLiveData<>();
 
-    public void fillList() {
-        list.add(new DataModel("Привет"));
-        list.add(new DataModel("qwert"));
-        list.add(new DataModel("asdfg"));
-        list.add(new DataModel("zxcvb"));
-        list.add(new DataModel("/.,mn"));
-        list.add(new DataModel("lkjhgf"));
-        mutableLiveData.setValue(list);
-    }
 }
